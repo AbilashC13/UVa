ecfa64163efab1b674b8fcccb1cf93662aed5ef5
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-01-15 11:40:01 +0000

    Create Relational Operator.java

---
+++ Relational Operator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String[] args)
+{
+
+    int a,b;
+Scanner sc=new Scanner(System.in);
+int t=in.nextInt();
+while(t-->0)
+{
+ a=sc.nextInt();
+ b=sc.nextInt();
+if(a>b)
+System.out.println(">");
+else if(a<b)
+System.out.println("<");
+else
+System.out.println("=");
+
+
+}
+}
+}
