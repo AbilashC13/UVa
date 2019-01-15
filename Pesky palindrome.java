eea1d4fc1ae3bd511ff164543048a4870f2422cc
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-01-15 15:12:28 +0000

    Create Pesky palindrome.java

---
+++ Pesky palindrome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNextLine())
+		{
+		    String s=sc.nextLine();
+		    HashSet<String> sub=new HashSet<String>();
+		    
+		    for(int i=0;i<s.length();i++)
+		    {
+		        for(int j=i+1;j<=s.length();j++)
+		        {
+		            StringBuilder sb1=new StringBuilder(s.substring(i,j));
+		            StringBuilder sb2=new StringBuilder(s.substring(i,j));
+		            if(sb1.toString().equals(sb2.reverse().toString()))
+		            sub.add(sb1.toString());
+		        }
+		    }
+		    System.out.println("The string "+"\'"+s+"\'"+" contains "+sub.size()+" palindromes.");
+        }
+	}
+}
