7e6b1d3664557087a1cdb18279574c3bd8d22d88
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-01-15 13:35:38 +0000

    Create Snail.java

---
+++ Snail.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            double H = sc.nextInt(), U = sc.nextInt() , 
+            D = sc.nextInt(), F = sc.nextInt();
+            if(H==0&&U==H&&D==0&&F==0){break;}
+            double d= 0;  
+            long days = 0;
+            F = (U*F)/100.0;
+            while(true){
+                days++; 
+                if(U>=0){
+                d+= U; } 
+                if(d>H){break;}
+                d-= D;  
+                if(d<0){break;}
+                U-= F;
+       
+            }
+        if(d>=0){System.out.println("success on day "+days);}  
+        else{System.out.println("failure on day " + days);}  
+        }
+        
+    }
+}
