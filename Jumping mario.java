1cc0fa555e1183660dd075b7951dc61590b08d90
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-01-21 13:41:37 +0530

    Create Jumping mario.java

---
+++ Jumping mario.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int tc=0;tc<t;tc++)
+		{
+			int n=sc.nextInt();
+			int c1=0,c2=0;
+			int[] a=new int[n];
+			for(int i=0;i<n;i++)
+			   a[i]=sc.nextInt();
+			for(int i=0;i<n-1;i++)
+			{
+				if(a[i]<a[i+1])
+				 c1++;
+			    else if(a[i]>a[i+1])
+			     c2++;
+			}
+			System.out.println("Case "+tc+": "+c1+" "+c2);
+		}
+	}
+}
