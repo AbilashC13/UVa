950081ca8a63ce9c43c40eed25e9cc324b2764db
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-01-25 16:17:18 +0530

    Update Generating fast.java

--- Generating fast.java
+++ Generating fast.java
@@ -15,32 +15,29 @@ for (int i =0; i<t ; i++) {
  Arrays.sort(a);
  do{
   bf.append(new String(a)+"\n");
- }while(next_permutation2(a));
+ }while(nextPermutation(a));
 }
 System.out.println(bf); 
   
- }
- public static boolean next_permutation2(char a[])
-    {
-       for(int i = a.length-2, j; i >= 0; i--)
-       {
-        if (a[i+1] > a[i])
-               {
-                for(j = a.length-1; a[j] <= a[i]; j--);
-   swap(a, i, j);
-      for(j = 1; j <= (a.length-i)/2; j++)
-       swap(a, i+j, a.length-j);
-                      return true;
-             }
+ }static boolean nextPermutation(char[] p) {
+        for (int a = p.length - 2; a >= 0; --a) {
+            if (p[a] < p[a + 1]) {
+                for (int b = p.length - 1;; --b) {
+                    if (p[b] > p[a]) {
+                        char t = p[a];
+                        p[a] = p[b];
+                        p[b] = t;
+                        for (++a, b = p.length - 1; a < b; ++a, --b) {
+                            t = p[a];
+                            p[a] = p[b];
+                            p[b] = t;
+                        }
+                        return true;
+                    }
+                }
+            }
         }
-         return false;
-   }
+        return false;
+    }
 
-
- public static void swap(char a[], int i, int j)
- {
-  char temp = a[i];
-  a[i] = a[j];
-   a[j] = temp;
-      }
 }
