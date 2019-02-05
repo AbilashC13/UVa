2491e2e38dab4d166dd768366e1706f12dbb9d7d
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-02-05 11:04:48 +0530

    Update CD.java

--- CD.java
+++ CD.java
@@ -3,19 +3,20 @@ import java.util.*;
 
 class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			int n = sc.nextInt(), m = sc.nextInt();
-			if (n + m == 0)
+		Scanner sc=new Scanner(System.in);
+		while(true){
+			int n=sc.nextInt(),m=sc.nextInt();
+			if (n+m==0)
 			      break;
 
-			Set<Integer> a = new TreeSet<Integer>();
-			for (int i = 0; i < n; i++) {
+			Set<Integer>a=new TreeSet<Integer>();
+			for (int i=0;i<n;i++)
+			{
 				a.add(sc.nextInt());
 			}
 
-			int c = 0;
-			for (int i = 0; i < m; i++) {
+			int c=0;
+			for (int i=0;i<m;i++) {
 				if (a.contains(sc.nextInt())) {
 					c++;
 				}
