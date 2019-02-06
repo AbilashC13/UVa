30e9f078d143de202d9d098d805f824e473ff1ee
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-02-06 15:25:00 +0530

    Update Conformity.java

--- Conformity.java
+++ Conformity.java
@@ -1,45 +1,39 @@
+import java.io.*;
 import java.util.*;
 public class Main
 {
-	public static void main(String args[])
-	{
-	Scanner sc=new Scanner(System.in);
-		while(true)
-		{
+	public static void main(String args[]){
+       Scanner sc=new Scanner(System.in);
+		while(sc.hasNextInt()){
 			int t=sc.nextInt();
 			if(t==0)
 				break;
-			int[] abc=new int[t];
-			for(int i=0;i<t;i++)
-			{
+			long[] abc=new long[t];
+			for(int i=0;i<t;i++){
 				int[] a=new int[5];
-				for(int j=0;j<5;j++)
-				{
+				for(int j=0;j<5;j++){
 					a[j]=sc.nextInt();
 				}
 				Arrays.sort(a);
-				int c=0;
-				for(int j=0;j<5;j++)
-				{
+				long c=0;
+				for(int j=0;j<5;j++){
 					c=c*1000+a[j];
 				}
 				abc[i]=c;
 			}
 			Arrays.sort(abc);
-			int curr=abc[0];
+			long curr=abc[0];
 			int count=1;
 			int maxcount=0;
 			int ans=0;
-			for(int i=1;i<t;i++)
-			{
+			for(int i=1;i<t;i++){
 				if(curr==abc[i])
 					count++;
 				else{
 					curr=abc[i];
 					if(maxcount==count)
 						ans+=count;
-					else if(maxcount<count)
-					{
+					else if(maxcount<count){
 						ans=count;
 						maxcount=count;
 					}
@@ -48,8 +42,7 @@ public class Main
 			}
 			if(maxcount==count)
 				ans+=count;
-			else if(maxcount<count)
-			{
+			else if(maxcount<count){
 				ans=count;
 			}
 			System.out.println(ans);
