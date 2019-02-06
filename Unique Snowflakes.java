2b1851e5d97032510052f248797eca49d85558ac
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-02-06 13:56:32 +0530

    Update Unique Snowflakes.java

--- Unique Snowflakes.java
+++ Unique Snowflakes.java
@@ -1,24 +1,42 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+
+class Main
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		while(t-->0)
-		{
-		int n=sc.nextInt();
-		HashSet<Integer> a=new HashSet<Integer>();
-		for(int i=0;i<n;i++)
-		   a.add(sc.nextInt());
-		System.out.println(a.size());
-		}
-	}
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t-->0)
+        {
+            int n=in.nextInt(),max=0,s=0;
+            int a[]=new int[n];
+            for(int i=0;i<n;i++)
+                a[i]=in.nextInt();
+            if(n>0)  {
+            HashMap<Integer,Integer> hs=new HashMap<>();
+            int b[]=new int[n];
+            b[0]=1;
+            hs.put(a[0],0);
+            for(int i=1;i<n;i++)
+            {
+                if(hs.containsKey(a[i]))
+                {
+                    int e=hs.get(a[i]);
+                    for(int j=s;j<=e;j++)
+                        hs.remove(a[j]);
+                    s=e+1;
+                    b[i]=i-s+1;
+                }
+                else
+                    b[i]=b[i-1]+1;
+                hs.put(a[i],i);
+            }
+            for (int i : b)
+                max=Math.max(i, max);
+            }
+            System.out.println(max);
+        }
+    }
 }
