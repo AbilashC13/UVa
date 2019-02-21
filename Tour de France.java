7a02ba7d81bfc31dd18a02674cca6437d47ae8ef
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-02-21 16:12:28 +0530

    Update Tour de France.java

--- Tour de France.java
+++ Tour de France.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Main
 {
-	public static void main (String[] args) throws NumberFormatException, IOException
+	public static void main (String[] args)
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
