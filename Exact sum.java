34608536be3851b7a1b40af6a1b320af0e4d6f6a
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-05-16 11:10:57 +0530

    Update Exact sum.java

--- Exact sum.java
+++ Exact sum.java
@@ -13,6 +13,43 @@ class Ideone
 		Scanner sc=new Scanner(System.in);
 		while(sc.hasNextInt())
 		{
+			int n=sc.nextInt();
+			int[] a=new int[n];
+			for(int i=0;i<n;i++)
+			    a[i]=sc.nextInt();
+			Arrays.sort(a);
+			int m=sc.nextInt();
+			int i=0,j=n-1;
+			int x=0,y=0;
+			while(i<j)
+			{
+				if(a[i]+a[j]<m)
+				    i++;
+				else if(a[i]+a[j]==m)
+				{
+					x=i;
+					y=j;
+					i++;j--;
+				}
+				else
+					j--;
+			}
+			System.out.println("Peter should buy books whose prices are "+a[x]+" and "+a[y]+".\n");
+		}
+	}
+}
+/*
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNextInt())
+		{
 		int n=sc.nextInt();
 		int[] a=new int[n];
 		for(int i=0;i<n;i++)
@@ -38,3 +75,4 @@ class Ideone
 		}
 	}
 }
+*/
